f5a53c5a251bbeccaba264c2d9089b2766e3d1f1
LightShed <84023057+lampshed@users.noreply.github.com>
2021-11-17 15:15:47 +0000

    Create config.js
    Replace the values with the annotated info.

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	accountName: 'accountUserName3000',
+	password: 'BOT acc pass',
+	sharedSecret: 'BOT acc shared secret here',
+	identSec: 'BOT acc ident Secret here',
+	owner: 'your main steam account link here'
+}
